64274ba5bb3a8c554c87de87b3998e4447d17d55
mohammadathania <61644013+mohammadathania@users.noreply.github.com>
2020-04-17 20:55:51 +0530

    Update dsip.m

--- dsip.m
+++ dsip.m
@@ -1,10 +1,16 @@
 load handel.mat
 filename = 'handel.wav';
-audiowrite(filename,y,Fs);
+audiowrite(filename,y,Fs,'BitsPerSample',8,...
+'Comment','This is my new audio file.');
 clear y Fs
 [y,Fs] = audioread('handel.wav');
-sound(y,Fs);
-y = y(:,1);
+y1 = uint8( (y + 1)/2 * 255 );
+%sound(y,Fs);
+y1 = y1(:,1);
 dt = 1/Fs;
 t = 0:dt:(length(y)*dt)-dt;
-plot(t,y); xlabel('Seconds'); ylabel('Amplitude');
+plot(t,y1); xlabel('Seconds'); ylabel('Amplitude');
+%variables using to find the probability
+cnt=1;
+sigma=0;
+whos y1;
